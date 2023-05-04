8f354d15cd02c8b9bcf26ebea0f6bbd77876f006
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-05-04 09:25:37 -0700

    style: added new separates in UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final List<User> users = new ArrayList<>();
     private long id;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<User> create(@Valid @RequestBody User user) {
@@ -25,6 +28,7 @@ public class UserController {
         log.info("Пользователь успешно добавлен. Текущее количество пользователей: " + users.size());
         return ResponseEntity.ok(user);
     }
+
     @PutMapping
     public ResponseEntity<User> update(@Valid @RequestBody User user) {
         User userToUpdate = users.stream().filter(user1 -> user1.getId() == user.getId()).findFirst().get();
