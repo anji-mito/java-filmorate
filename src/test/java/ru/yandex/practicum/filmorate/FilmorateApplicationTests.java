81988540d43916a1bc9cf827666d815878ed4e79
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-06-12 05:37:54 -0700

    style: Tests

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -5,48 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.user.UserService;
 
-import java.time.LocalDate;
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
-    private final UserService userService;
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    public void testFindUserById() {
-        Optional<User> userOptional = Optional.ofNullable(userService.getUser(1L));
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1L)
-                );
-    }
-
-    @Test
-    public void createUser() {
-        User userTest;
-        userTest = User.builder()
-                .email("mail@mail.ru")
-                .birthday(LocalDate.now())
-                .login("login")
-                .name("name")
-                .build();
-        Optional<User> userOptional = Optional.ofNullable(userService.create(userTest));
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1L)
-                );
     }
 }
